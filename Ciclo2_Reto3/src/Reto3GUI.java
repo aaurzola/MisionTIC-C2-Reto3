4d9727d45dfe77bf04ee5dc41215a64bfeb9253d
Alejandro <aaurzola@gmail.com>
2022-06-09 17:54:16 -0500

    GUI leftside structure almost completed, need to know where to display the output results

--- Ciclo2_Reto3/src/Reto3GUI.java
+++ Ciclo2_Reto3/src/Reto3GUI.java
@@ -5,6 +5,12 @@ public class Reto3GUI extends JFrame{
     private JButton button;
     private JButton procesarButton;
     private JTable table1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
 
     public Reto3GUI(String title) {
         super(title);
