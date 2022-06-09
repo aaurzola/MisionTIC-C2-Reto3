4382ce7c5bea49d62a3165cb916a8b177bee5c22
Alejandro <aaurzola@gmail.com>
2022-06-09 17:17:11 -0500

    GUI main structure

--- Ciclo2_Reto3/src/Reto3GUI.java
+++ Ciclo2_Reto3/src/Reto3GUI.java
@@ -1,5 +1,21 @@
 import javax.swing.*;
 
-public class Reto3GUI {
-    private JLabel output;
+public class Reto3GUI extends JFrame{
+    private JPanel mainPanel;
+    private JButton button;
+    private JButton procesarButton;
+    private JTable table1;
+
+    public Reto3GUI(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new Reto3GUI("Reto3 Alejandro Urzola");
+        frame.setVisible(true);
+    }
+
 }
