5c3b51d8b14c744ab07985018304adf855dc91c8
Alejandro <aaurzola@gmail.com>
2022-06-11 02:53:28 -0500

    finished project with minimun requirements | data entry validation still needed

--- Ciclo2_Reto3/src/Reto3GUI.java
+++ Ciclo2_Reto3/src/Reto3GUI.java
@@ -1,13 +1,8 @@
 import model.Paciente;
 
 import javax.swing.*;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.awt.event.*;
+import java.util.*;
 
 public class Reto3GUI extends JFrame{
     private JPanel mainPanel;
@@ -20,8 +15,8 @@ public class Reto3GUI extends JFrame{
     private JTextField cedulaTxt;
     private JScrollPane scroll;
     private JList userList;
-    private JPanel rightPanel;
     private JPanel leftPanel;
+    private JPanel rightPanel;
     private JPanel inputPanel;
     private JPanel outputPanel;
     private JPanel btnPanel;
@@ -33,15 +28,34 @@ public class Reto3GUI extends JFrame{
     private JPanel formDataLeftPanel;
     private JPanel formDataRightPanel;
     private JLabel resultsLbl;
+    private JButton randomUserBtn;
+    private JLabel counterLbl;
+    private JButton resetBtn;
 
     public Reto3GUI(String title) {
-        super(title); //calls super constructor,
+        super(title); //calls super constructor
 
         //global variables
-        DefaultListModel listModel = new DefaultListModel();
+        DefaultListModel listDataUsersModel = new DefaultListModel();
+        DefaultListModel listResultModel = new DefaultListModel();
         ArrayList<Paciente> paciente = new ArrayList<Paciente>();
         Map<String, Integer> ciudadesMap = new LinkedHashMap<String, Integer>();
 
+        //dataset for user randomizer
+        String[] randomNames = new String[] {
+                "Cornelia", "Peggy", "Christopher", "Peter", "Gertrude", "Francis",
+                "Richard", "Lawrence", "Lionel", "Arnold", "Julio", "Helen"}; //length = 12
+        String[] randomLastNames = new String[] {
+                "Coughlin", "Short", "Lewis", "Sanchez", "Martinez", "Lund",
+                "Blunt", "Leab", "Williams", "Burns", "Hacker", "Carraway"}; //length = 12
+        String[] randomEps = new String[] {
+                "SURA", "Sanitas", "Comfenalco", "Cafesalud", "Coomeva", "Compensar",
+                "SALUDCOLOMBIA", "Salud Total", "Solsalud", "Saludcoop", "Cruz Blanca", "Salud Colmena"}; //length = 12
+        String[] randomCities = new String[] {
+                "Inirida", "Rioacha", "Mocoa", "Armenia", "Medellin", "Barranquilla",
+                "Sincelejo", "Villavicencio", "Buracamanga", "Cartagena", "Ibague", "Pereira"}; //length = 12
+        String[] randomIllness = new String[] {"Cancer", "Cardiovasculares", "Respiratorias",
+                "Cerebrovasculares", "Hipertension", "Diabetes"}; //length 6
 
         //misc for UI
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -49,21 +63,14 @@ public class Reto3GUI extends JFrame{
         this.pack();
 
         //event listeners
-
-        //TODO create eventListeners for buttons and inputs
         //TODO create validations for user interactions
-        addBtn.addMouseListener(new MouseAdapter() {
+        addBtn.addActionListener(new ActionListener() {
             @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
+            public void actionPerformed(ActionEvent e) {
                 Paciente persona = new Paciente(nombreTxt.getText(), Long.parseLong(cedulaTxt.getText()), Integer.parseInt(edadTxt.getText()), ciudadTxt.getText(), epsTxt.getText(), (String) illnessCmb.getSelectedItem());
                 paciente.add(persona);
-                for (int i = 0; i < 50; i++) {
-                    listModel.addElement(persona.toString('-'));
-                }
-                nombreTxt.setText("");
-                userList.setModel(listModel);
-                resultsList.setModel(listModel);
+                listDataUsersModel.addElement(persona.toString('-'));
+                userList.setModel(listDataUsersModel);
 
                 //verificar si la ciudad hace parte del Map de ciudades.
                 if (ciudadesMap.containsKey(persona.getCiudad())) {
@@ -72,7 +79,16 @@ public class Reto3GUI extends JFrame{
                 } else {
                     ciudadesMap.put(persona.getCiudad(), 1); //indexa nueva ciudad con contador en 1
                 }
-
+                nombreTxt.setText("");
+                cedulaTxt.setText("");
+                edadTxt.setText("");
+                ciudadTxt.setText("");
+                epsTxt.setText("");
+                illnessCmb.setSelectedItem("Seleccionar");
+                counterLbl.setText(String.valueOf(listDataUsersModel.size()));
+                addBtn.setEnabled(false);
+                resetBtn.setEnabled(true);
+                processBtn.setEnabled(true);
             }
         });
         nombreTxt.addFocusListener(new FocusAdapter() {
@@ -82,12 +98,95 @@ public class Reto3GUI extends JFrame{
 //                JOptionPane.showMessageDialog(null, "test");
             }
         });
+        randomUserBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                Random rand = new Random();
+
+                //random fore name and last name
+                nombreTxt.setText(randomNames[rand.nextInt(12)] + " " + randomLastNames[rand.nextInt(12)]);
+
+                //random ID number
+                long min = 1100000000;
+                long max = 1200000000;
+                long random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+                cedulaTxt.setText(String.valueOf(random_int));
+
+                //random age
+                edadTxt.setText(String.valueOf(21 + rand.nextInt(80)));
+
+                //random city
+                ciudadTxt.setText(randomCities[rand.nextInt(12)]);
+
+                //random Health Promotion Agency
+                epsTxt.setText(randomEps[rand.nextInt(12)]);
+
+                //random illness
+                illnessCmb.setSelectedItem(randomIllness[rand.nextInt(5)]);
+
+                //enable addBtn
+                addBtn.setEnabled(true);
+            }
+        });
+        processBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                listResultModel.clear();
+
+                //imprime pacientes por ciudades
+                listResultModel.addElement("1. CANTIDAD DE PACIENTES POR CIUDAD:");
+                for (String key : ciudadesMap.keySet()) {
+                    listResultModel.addElement(key + " " + ciudadesMap.get(key));
+                    System.out.println(key + " " + ciudadesMap.get(key));
+                }
+                resultsList.setModel(listResultModel);
+
+                //imprime primera ciudad con menor cantidad de pacientes
+                listResultModel.addElement(" ");
+                listResultModel.addElement("2. CIUDAD CON MENOR CANTIDAD DE PACIENTES:");
+                int min = Collections.min(ciudadesMap.values());
+                for(String key : ciudadesMap.keySet()){
+                    if (ciudadesMap.get(key) == min) {
+                        System.out.println(key);
+                        listResultModel.addElement(key);
+                        break;
+                    }
+                }
+
+                //imprime pacientes de tercera edad
+                listResultModel.addElement(" ");
+                listResultModel.addElement("3. PACIENTES REGISTRADOS DE TERCERA EDAD:");
+                for (int i = 0; i < paciente.size(); i++) {
+                    if (paciente.get(i).clasificarEdad().equalsIgnoreCase("Tercera edad")) {
+                        System.out.println(paciente.get(i).getNombre() + " " + paciente.get(i).getCedula());
+                        listResultModel.addElement(paciente.get(i).getNombre() + " " + paciente.get(i).getCedula());
+                    }
+                }
+                if (listResultModel.lastElement() == "3. PACIENTES REGISTRADOS DE TERCERA EDAD:") {
+                    listResultModel.addElement("No hay pacientes de tercera edad registrados");
+                }
+                processBtn.setEnabled(false);
+            }
+        });
+        resetBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                listDataUsersModel.clear();
+                listResultModel.clear();
+                resetBtn.setEnabled(false);
+                processBtn.setEnabled(false);
+                counterLbl.setText("0");
+                paciente.clear();
+                ciudadesMap.clear();
+            }
+        });
     }
 
 
     public static void main(String[] args) {
         JFrame frame = new Reto3GUI("Reto3 Alejandro Urzola");
-        frame.setSize(1200, 650);
+        frame.setSize(1200, 700);
         frame.setVisible(true);
     }
 }
